cca0ac04e10f91e1b44280e24469f1e6fdff2591
Joselito Machado <84118345+joselitomachado@users.noreply.github.com>
2023-10-15 19:55:06 -0300

    Adicionar arquivos de projeto.

---
+++ DigiBank/Contratos/IConta.cs
@@ -0,0 +1,15 @@
+﻿using DigiBank.Classes;
+
+namespace DigiBank.Contratos
+{
+    public interface IConta
+    {
+        void Deposito(double valor);
+        bool Saque(double valor);
+        double ConsultaSaldo();
+        string GetCodigoDoBanco();
+        string GetNumeroAgencia();
+        string GetNumeroDaConta();
+        List<Extrato> Extrato();
+    }
+}
